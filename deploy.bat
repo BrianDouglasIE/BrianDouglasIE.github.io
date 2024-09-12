b18ae2638f3262ee5a2495ccd05b9029b06fd017
BrianDouglasIE <briandgls92@gmail.com>
2024-09-12 13:05:15 +0100

    ++

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
 git fetch origin
-git subtree pull --prefix dist origin gh-pages --allow-unrelated-histories
+git subtree pull --prefix dist origin gh-pages --squash
 git subtree push --prefix dist origin gh-pages
