38b9abb458843f38c0230f61ab1f2d0b64860579
BrianDouglasIE <briandgls92@gmail.com>
2025-05-08 10:56:15 +0100

    feat: remove tracking

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,30 +1,3 @@
-async function sendPostRequest(path, title) {
-  const url =
-    "https://analytics.atlantic-design.ie/websites/1/record-page-visit";
-
-  const payload = {
-    path,
-    title,
-  };
-
-  try {
-    await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Referer: "https://briandouglas.ie",
-      },
-      body: JSON.stringify(payload),
-    });
-  } catch (error) {
-    throw error;
-  }
-}
-
 window.addEventListener("load", () => {
-  const path = window.location.pathname;
-  const title = document.title;
-  sendPostRequest(path || "/", title)
-    .then((response) => console.log("Thank you for visiting"))
-    .catch((error) => console.error("Failure:", error));
+    console.log("Thank you for visiting")
 });
